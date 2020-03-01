70d221885cae61129fe9f228ab28544bfb2047ce
Terentev Kirill <mowder96@gmail.com>
2020-03-01 22:48:11 +0500

    PlatformController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,9 +8,12 @@ public class GameController : MonoBehaviour
 
 	[SerializeField]
 	private Rigidbody2D _ball;
+	[SerializeField]
+	private PlatformController[] _platforms;
 
 	private InputController _inputController;
 	private Vector2? _forceDirection;
+	private int _bounceCount = 0;
 
 	private void Awake()
 	{
@@ -18,9 +21,17 @@ public class GameController : MonoBehaviour
 		_inputController = gameObject.AddComponent<MouseController>();
 #endif
 
-#if UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
 		_inputController = gameObject.AddComponent<TouchController>();
 #endif
+	}
+
+	private void Start()
+	{
+		foreach (var platform in _platforms)
+		{
+			platform.CollisionEvent.AddListener(IncrementBounceCount);
+		}
 	}
 
 	private void Update()
@@ -31,4 +42,9 @@ public class GameController : MonoBehaviour
 			_ball.AddForce((Vector2)_forceDirection * ForceSensitivity, ForceMode2D.Force);
 	    }
     }
+
+	private void IncrementBounceCount()
+	{
+		_bounceCount++;
+	}
 }
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PlatformController : MonoBehaviour
+{
+	[HideInInspector]
+	public UnityEvent CollisionEvent;
+
+	private Image _image;
+
+	private void Awake()
+	{
+		_image = GetComponent<Image>();
+		CollisionEvent = new UnityEvent();
+	}
+
+	private void SetRandomColor()
+	{
+		Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
+		_image.color = color;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		SetRandomColor();
+		CollisionEvent?.Invoke();
+	}
+}
